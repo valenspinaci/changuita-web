a6442893ad513b84e4c5f3ecd28a76ce0d9a40ef
Valentino Spinaci <spinacivalentino@gmail.com>
2026-06-07 12:57:50 -0300

    acomodando import de css

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { AuthProvider } from './context/AuthContext'
 import App from './App';
-
+import './index.css'
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
